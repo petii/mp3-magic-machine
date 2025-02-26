509651a70cb58e3ba3b763888326a50ad6c2ce83
Péter Vida <peter.von.vida@gmail.com>
2025-02-26 22:34:34 +0100

    fmt

--- magic_lambda/src/event_handler.rs
+++ magic_lambda/src/event_handler.rs
@@ -1,12 +1,12 @@
-use lambda_runtime::{tracing, Error, LambdaEvent};
 use aws_lambda_events::event::s3::S3Event;
+use lambda_runtime::{tracing, Error, LambdaEvent};
 
 /// This is the main body for the function.
 /// Write your code inside it.
 /// There are some code example in the following URLs:
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
 /// - https://github.com/aws-samples/serverless-rust-demo/
-pub(crate)async fn function_handler(event: LambdaEvent<S3Event>) -> Result<(), Error> {
+pub(crate) async fn function_handler(event: LambdaEvent<S3Event>) -> Result<(), Error> {
     // Extract some useful information from the request
     let payload = event.payload;
     tracing::info!("Payload: {:?}", payload);
--- magic_lambda/src/main.rs
+++ magic_lambda/src/main.rs
@@ -3,7 +3,6 @@ use lambda_runtime::{run, service_fn, tracing, Error};
 mod event_handler;
 use event_handler::function_handler;
 
-
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     tracing::init_default_subscriber();
