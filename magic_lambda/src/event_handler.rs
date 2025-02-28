9fc0272210bd6945915fe0a16bbe25d111ad433f
Péter Vida <peter.von.vida@gmail.com>
2025-02-28 02:57:44 +0100

    try to upload resulting files

--- magic_lambda/src/event_handler.rs
+++ magic_lambda/src/event_handler.rs
@@ -1,12 +1,17 @@
 use aws_lambda_events::event::s3::S3Event;
-use aws_lambda_events::s3::{S3Entity, S3EventRecord};
+use aws_lambda_events::s3::{S3Bucket, S3Entity, S3EventRecord, S3Object};
 use aws_sdk_s3::operation::get_object::GetObjectOutput;
+use aws_sdk_s3::primitives::ByteStream;
 use channel_io::ChannelReader;
 use hound::WavReader;
 use lambda_runtime::{tracing, Error, LambdaEvent};
+use std::path::{Path, PathBuf};
 use tokio::task::JoinHandle;
 
-async fn handle_s3_object(get_object_output: GetObjectOutput) -> Result<(), Error> {
+async fn handle_s3_object(
+    get_object_output: GetObjectOutput,
+    result_path_sender: flume::Sender<PathBuf>,
+) -> Result<(), Error> {
     let mut object_body = get_object_output.body;
 
     let (tx, rx) = flume::unbounded();
@@ -24,10 +29,20 @@ async fn handle_s3_object(get_object_output: GetObjectOutput) -> Result<(), Erro
 
     tracing::info!("opened file with wav spec: {:?}", reader.spec());
 
-    // match reader.spec.bits_per_sample {
-    //     16 => tracing::debug!("this is the part where the conversion should happen"),
-    //     _ => tracing::error!("not 16 bits per sample"),
-    // }
+    match reader.spec().bits_per_sample {
+        16 => {
+            tracing::debug!("this is the part where the conversion should happen");
+            let files = wav_to_mp3::mp3_encode_i16::from_reader(
+                reader,
+                Path::new("/tmp"),
+                "smarter-naming",
+            );
+            for file in files {
+                result_path_sender.send(file)?;
+            }
+        }
+        _ => tracing::error!("not 16 bits per sample"),
+    }
 
     body_bytestream_reader_handle.await??;
 
@@ -66,24 +81,46 @@ pub(crate) async fn function_handler(event: LambdaEvent<S3Event>) -> Result<(), 
 
         tracing::info!("at {event_time}: s3 entity = {:?}", event_entity);
 
-        let S3Entity { bucket, object, .. } = event_entity;
+        let S3Entity {
+            bucket: S3Bucket {
+                name: bucket_name, ..
+            },
+            object: S3Object { key, .. },
+            ..
+        } = event_entity;
 
         let get_object_result = s3_client
             .get_object()
-            .bucket(bucket.name.unwrap())
-            .key(object.key.unwrap())
+            .bucket(bucket_name.clone().unwrap())
+            .key(key.clone().unwrap())
             .send()
-            .await;
+            .await?;
 
-        match get_object_result {
-            Ok(output) => {
-                tracing::info!("get_object() = {:?}", output);
-                handle_s3_object(output).await?;
-            }
-            Err(error) => {
-                tracing::error!("get_object() = {:?}", error);
-                return Err(error.into());
-            }
+        tracing::info!("get_object() = {:?}", get_object_result);
+
+        let (tx, rx) = flume::unbounded();
+
+        handle_s3_object(get_object_result, tx).await?;
+
+        while let Ok(file) = rx.recv() {
+            tracing::info!("received written file = {:?}", file);
+            s3_client
+                .put_object()
+                .bucket(bucket_name.clone().unwrap())
+                .key(format!(
+                    "{}-{}",
+                    key.clone().unwrap(),
+                    file.components()
+                        .last()
+                        .unwrap()
+                        .as_os_str()
+                        .to_os_string()
+                        .into_string()
+                        .unwrap()
+                ))
+                .body(ByteStream::from_path(file).await?)
+                .send()
+                .await?;
         }
     }
 
